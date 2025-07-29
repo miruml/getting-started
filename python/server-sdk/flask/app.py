8dadefeb96b8458655f0c56a6ba6acbce94739d8
Benjamin Smidt <ben@miruml.com>
2025-07-29 07:15:24 -0700

    refactor: sdk to validate webhooks instead of callbacks

--- python/server-sdk/flask/app.py
+++ python/server-sdk/flask/app.py
@@ -1,46 +1,39 @@
 from flask import Flask, request, jsonify, Response
-from miru_server_sdk.callbacks import Callback, CallbackVerificationError
+from miru_server_sdk.webhooks import Webhook, WebhookVerificationError
 
 app = Flask(__name__)
 
 # do not place secrets directly in code, this is only for demo purposes
-SECRET = "cbsec_WycPjK+LHQw5hbeL1qcLOGjXsIWYuGzEUrwUZWNz4W4="
+SECRET = "whsec_XRmIwf3AIzFKZaW1TrJv02gl1UZMXBHK"
 
 
-@app.route("/callback", methods=["POST"])
-def callback_endpoint() -> tuple[Response, int]:
-    print("Callback received")
+@app.route("/webhooks/miru", methods=["POST"])
+def webhook_endpoint() -> tuple[Response, int]:
+    print("Webhook received")
 
     headers = dict(request.headers)
     payload = request.get_data()
 
-    # verify the callback
     try:
-        cb = Callback(SECRET)
-        cfgInstDeployment = cb.verify(payload, headers)
-    except CallbackVerificationError as e:
+        wh = Webhook(SECRET)
+        webhookPayload = wh.verify(payload, headers)
+    except WebhookVerificationError as e:
         return jsonify({
             'valid': False,
             'message': str(e),
             'errors': []
         }), 400
 
-    # TODO: check if the config instances being deployed are valid
-    print(cfgInstDeployment)
+    # TODO: check if the webhook payload is valid
+    print(webhookPayload)
 
     # return a valid response
-    return jsonify({
-        'valid': True,
-        'message': 'valid config instance deployment',
-        'errors': []
-    }), 200
+    return jsonify({'message': 'webhook successfully received'}), 200
 
 
 @app.route("/", methods=["GET"])
 def health_check() -> tuple[Response, int]:
-    return jsonify({
-        'message': 'ok'
-    }), 200
+    return jsonify({'message': 'ok'}), 200
 
 
 if __name__ == "__main__":
